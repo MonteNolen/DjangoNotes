8900a341ba35780e3963b3c924bcf5a8a24b4667
MonteNolen <gavalost@ukr.net>
2022-02-13 14:37:25 +0300

    1

--- DjangoNotes/settings.py
+++ DjangoNotes/settings.py
@@ -122,10 +122,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 STATICFILES_DIRS = [
     BASE_DIR / "notes/static",
     #'/var/www/static/',
 ]
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
